0ffac27f611475a11eab034231cc323819fc7f90
kajj8808 <a01039388808@gmail.com>
2024-06-11 06:24:59 +0900

    ✅ 불러오는 범위를 일주일 단위로 변

--- index.ts
+++ index.ts
@@ -22,58 +22,63 @@ interface AnissiaDetail {
 }
 
 async function autoSaveAnissiaDataToDB() {
-  const nowDayOfWeek = new Date().getDay();
-  const anissiaDayAnimations = (await (
-    await fetch(`${ANISSIA_API_URL}/schedule/${nowDayOfWeek}`)
-  ).json()) as { code: string; data: DayAnimation[] };
+  // const nowDayOfWeek = new Date().getDay();
+  // 0-6 요일 | 7 기타 | 8 신작
+  for (let day = 0; day <= 6; day++) {
+    const anissiaDayAnimations = (await (
+      await fetch(`${ANISSIA_API_URL}/schedule/${day}`)
+    ).json()) as { code: string; data: DayAnimation[] };
 
-  if (
-    anissiaDayAnimations.code === "ok" &&
-    anissiaDayAnimations.data.length > 1
-  ) {
-    for (let anissiaAnimation of anissiaDayAnimations.data) {
-      await db.animation.upsert({
-        where: {
-          animeNo: anissiaAnimation.animeNo,
-        },
-        update: {
-          status: anissiaAnimation.status,
-        },
-        create: {
-          animeNo: anissiaAnimation.animeNo,
-          name: anissiaAnimation.subject,
-          status: anissiaAnimation.status,
-          website: anissiaAnimation.website,
-          genres: anissiaAnimation.genres,
-        },
-      });
-      const anissiaAnimationDetails = (await (
-        await fetch(
-          `${ANISSIA_API_URL}/caption/animeNo/${anissiaAnimation.animeNo}`
-        )
-      ).json()) as { code: string; data: AnissiaDetail[] };
-      if (
-        (anissiaAnimationDetails.code === "ok",
-        anissiaAnimationDetails.data.length >= 1)
-      ) {
-        for (let anissiaAnimationDetail of anissiaAnimationDetails.data) {
-          const isVaild = !Boolean(
-            await db.subTitle.findFirst({
-              where: {
-                website: anissiaAnimationDetail.website,
-              },
-            })
-          ); // 중복이 없을 경우 유효하다 보고 db에 item 추가
-          if (isVaild) {
-            await db.subTitle.create({
-              data: {
-                episode: anissiaAnimationDetail.episode,
-                uploader: anissiaAnimationDetail.name,
-                website: anissiaAnimationDetail.website,
-                updateAt: new Date(anissiaAnimationDetail.updDt),
-                animeNo: anissiaAnimation.animeNo,
-              },
-            });
+    if (
+      anissiaDayAnimations.code === "ok" &&
+      anissiaDayAnimations.data.length > 1
+    ) {
+      for (let anissiaAnimation of anissiaDayAnimations.data) {
+        await db.animation.upsert({
+          where: {
+            animeNo: anissiaAnimation.animeNo,
+          },
+          update: {
+            status: anissiaAnimation.status,
+          },
+          create: {
+            animeNo: anissiaAnimation.animeNo,
+            name: anissiaAnimation.subject,
+            status: anissiaAnimation.status,
+            website: anissiaAnimation.website,
+            genres: anissiaAnimation.genres,
+          },
+        });
+        const anissiaAnimationDetails = (await (
+          await fetch(
+            `${ANISSIA_API_URL}/caption/animeNo/${anissiaAnimation.animeNo}`
+          )
+        ).json()) as { code: string; data: AnissiaDetail[] };
+        if (
+          (anissiaAnimationDetails.code === "ok",
+          anissiaAnimationDetails.data.length >= 1)
+        ) {
+          for (let anissiaAnimationDetail of anissiaAnimationDetails.data) {
+            const isVaild =
+              !Boolean(
+                await db.subTitle.findFirst({
+                  where: {
+                    website: anissiaAnimationDetail.website,
+                  },
+                })
+              ) && anissiaAnimationDetail.website !== "";
+            // 중복이 아니거나 website가 있는 경우 유효하다 보고 db에 item 추가
+            if (isVaild) {
+              await db.subTitle.create({
+                data: {
+                  episode: anissiaAnimationDetail.episode,
+                  uploader: anissiaAnimationDetail.name,
+                  website: anissiaAnimationDetail.website,
+                  updateAt: new Date(anissiaAnimationDetail.updDt),
+                  animeNo: anissiaAnimation.animeNo,
+                },
+              });
+            }
           }
         }
       }
