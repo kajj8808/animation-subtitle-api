97f75214553cea4f3a2e19d5a6de65e875a03477
kajj8808 <a01039388808@gmail.com>
2024-06-11 13:55:04 +0900

    ✅ express get(데이터 가져오는부분) 작성

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,47 @@
-// import "./anissia";
+import "./anissia";
 import exporss from "express";
 import cors from "cors";
+import http from "http";
+import db from "../util/db";
+import "dotenv";
+
+const app = exporss();
+app.use(cors());
+
+app.get("/", async (_, res) => {
+  const animations = await db.animation.findMany({
+    include: {
+      _count: {
+        select: {
+          SubTitle: true,
+        },
+      },
+    },
+    take: 10,
+  });
+  res.json({
+    ok: true,
+    data: animations,
+  });
+});
+
+app.get("/:animeNo", async (req, res) => {
+  const { animeNo } = req.params;
+  if (animeNo) {
+    const subtitles = await db.subTitle.findMany({
+      where: {
+        animeNo: +animeNo,
+      },
+    });
+    res.json({ ok: true, data: subtitles });
+  }
+  res.status(404).end();
+});
+
+const httpServer = http.createServer(app);
+
+function handleListen() {
+  console.log(`server is ready http://localhost:${process.env.PORT}`);
+}
+
+httpServer.listen(process.env.PORT, handleListen);
